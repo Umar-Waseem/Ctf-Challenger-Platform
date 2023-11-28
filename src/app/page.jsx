b7ebd63d5644ce2ed5c548945866c8d966fa7fd9
Umar-Waseem <umar.waseem@gmail.com>
2023-11-28 23:02:34 +0500

    simple user interface for home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
-}
+"use client";
+
+import Sidebar from "../components/sidebar";
+import HomeArea from "../components/homeArea";
+
+const HomePage = () => {
+   return (
+      <>
+         <Sidebar sideBarItems={["Reverse Engineering", "Forensics", "Web Hacking", "OSINT", "Network Security"]} />
+         <HomeArea />
+      </>
+   );
+};
+
+export default HomePage;
---
+++ src/components/homeArea.jsx
@@ -0,0 +1,7 @@
+export default function HomeArea() {
+  return (
+    <div>
+
+    </div>
+  );
+}
