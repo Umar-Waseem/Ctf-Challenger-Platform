c8a2f71e8598d898091282643d06d269d0a4da45
Umar-Waseem <umar.waseem@gmail.com>
2023-11-29 20:55:38 +0500

    loading effect for auth

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,17 +3,15 @@
 import Link from "next/link";
 import { useState } from "react";
 import AuthError from "../../components/authError";
-
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function Login() {
-
-    const supabase = createClientComponentClient()
+    const supabase = createClientComponentClient();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         setError("");
@@ -24,34 +22,42 @@ export default function Login() {
             return;
         }
 
-        const { user, error } = await supabase.auth.signInWithPassword({
-            email,
-            password,
-        });
+        setLoading(true); // Set loading state to true
 
-        console.log(user, error)
+        try {
+            const { user, error } = await supabase.auth.signInWithPassword({
+                email,
+                password,
+            });
 
-        if (error) {
-            setError(error.message)
+            console.log(user, error);
+
+            if (error) {
+                setError(error.message);
+            } else {
+                window.location.href = "/";
+            }
+        } catch (error) {
+            console.error("Error signing in:", error.message);
+            setError("Error signing in. Please try again.");
+        } finally {
+            setLoading(false); // Set loading state back to false
         }
-        else {
-            window.location.href = "/"
-        }
-    }
+    };
 
     return (
-        <section class="bg-gray-50 dark:bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" action="#">
                             <div>
                                 <label
-                                    for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 >
                                     Your email
                                 </label>
@@ -60,15 +66,15 @@ export default function Login() {
                                     type="email"
                                     name="email"
                                     id="email"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="name@company.com"
                                     required=""
                                 />
                             </div>
                             <div>
                                 <label
-                                    for="password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 >
                                     Password
                                 </label>
@@ -78,7 +84,7 @@ export default function Login() {
                                     name="password"
                                     id="password"
                                     placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     required=""
                                 />
                             </div>
@@ -88,16 +94,18 @@ export default function Login() {
                             <button
                                 onClick={handleSubmit}
                                 type="submit"
-                                className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                className={`w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${loading ? 'opacity-50 cursor-not-allowed' : ''
+                                    }`}
+                                disabled={loading}
                             >
-                                Sign in
+                                {loading ? 'Signing in...' : 'Sign in'}
                             </button>
 
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don't have an account yet?{" "}
                                 <Link
                                     href="/signup"
-                                    class="font-medium hover:underline dark:text-white"
+                                    className="font-medium hover:underline dark:text-white"
                                 >
                                     Sign up
                                 </Link>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,22 +1,18 @@
 "use client"
 
 import Link from "next/link";
-
 import { useState } from "react";
 import AuthError from "../../components/authError";
-
-
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function SignUp() {
-
-    const supabase = createClientComponentClient()
+    const supabase = createClientComponentClient();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setName] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         setError("");
@@ -27,36 +23,43 @@ export default function SignUp() {
             return;
         }
 
-        const { user, error } = await supabase.auth.signUp({
-            email,
-            password,
+        setLoading(true); // Set loading state to true
 
-        })
+        try {
+            const { user, error } = await supabase.auth.signUp({
+                email,
+                password,
+            });
 
-        console.log(user, error)
+            console.log(user, error);
 
-        if (error) {
-            setError(error.message)
+            if (error) {
+                setError(error.message);
+            } else {
+                window.location.href = "/";
+            }
+        } catch (error) {
+            console.error("Error signing up:", error.message);
+            setError("Error signing up. Please try again.");
+        } finally {
+            setLoading(false); // Set loading state back to false
         }
-        else {
-            window.location.href = "/"
-        }
-    }
+    };
 
     return (
         <div>
-            <section class="bg-gray-50 dark:bg-gray-900">
-                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Register For An Account
                             </h1>
-                            <form class="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label
-                                        for="email"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        htmlFor="username"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                     >
                                         Your username
                                     </label>
@@ -65,15 +68,15 @@ export default function SignUp() {
                                         type="text"
                                         name="username"
                                         id="username"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="Umar Waseem"
                                         required=""
                                     />
                                 </div>
                                 <div>
                                     <label
-                                        for="email"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                     >
                                         Your email
                                     </label>
@@ -82,15 +85,15 @@ export default function SignUp() {
                                         type="email"
                                         name="email"
                                         id="email"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="name@company.com"
                                         required=""
                                     />
                                 </div>
                                 <div>
                                     <label
-                                        for="password"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        htmlFor="password"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                     >
                                         Password
                                     </label>
@@ -100,26 +103,27 @@ export default function SignUp() {
                                         name="password"
                                         id="password"
                                         placeholder="••••••••"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required=""
                                     />
                                 </div>
 
                                 <AuthError error={error} />
 
-
                                 <button
                                     onClick={handleSubmit}
                                     type="submit"
-                                    className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                    className={`w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${loading ? 'opacity-50 cursor-not-allowed' : ''
+                                        }`}
+                                    disabled={loading}
                                 >
-                                    Register
+                                    {loading ? 'Signing up...' : 'Register'}
                                 </button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Already have an account?{" "}
                                     <Link
                                         href="/login"
-                                        class="font-medium hover:underline dark:text-white"
+                                        className="font-medium hover:underline dark:text-white"
                                     >
                                         Login
                                     </Link>
