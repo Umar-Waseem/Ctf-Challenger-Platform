c3a99f0c9b1dfe0f87fd28454dde90f7e0d3e323
aifu <huzaifabilal226@gmail.com>
2023-12-03 13:23:55 +0500

    main-ui-change

--- src/components/homeArea.jsx
+++ src/components/homeArea.jsx
@@ -1,180 +1,175 @@
 
 // import EventCard from '../components/eventCard';
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import React from "react";
+import { Card, CardBody, CardFooter, Image, CardHeader, Divider, Link } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, getKeyValue } from "@nextui-org/react";
+import { users } from "./data";
 const list = [
   {
-    title: "Orange",
+    title: "WEB",
     img: "/images/fruit-1.jpeg",
     no_of_qs: "12",
   },
   {
-    title: "Tangerine",
+    title: "CRYPTO",
     img: "/images/fruit-2.jpeg",
     no_of_qs: "7",
   },
   {
-    title: "Raspberry",
+    title: "REVERSE",
     img: "/images/fruit-3.jpeg",
     no_of_qs: "8",
   },
   {
-    title: "Lemon",
+    title: "FORENSICS",
     img: "/images/fruit-4.jpeg",
     no_of_qs: "30",
   },
-  {
-    title: "Avocado",
-    img: "/images/fruit-5.jpeg",
-    no_of_qs: "10",
-  },
+  // {
+  //   title: "MOBILE",
+  //   img: "/images/fruit-5.jpeg",
+  //   no_of_qs: "10",
+  // },
 ];
 
 export default function HomeArea() {
+  const [page, setPage] = React.useState(1);
+  const rowsPerPage = 4;
+
+  const pages = Math.ceil(users.length / rowsPerPage);
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return users.slice(start, end);
+  }, [page, users]);
   return (
-    <div className="w-full px-4 sm:px-8 md:px-16 py-8 sm:py-12 md:py-16 bg-[#2D3250]">
-      <div className="grid gap-6 sm:gap-8 md:gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {list.map((item, index) => (
-          <Card
-            className="bg-[#1e243a] hover:bg-[#12172a] shadow-2xl"
-            radius="sm"
-            shadow="lg"
-            key={index}
-            isPressable
-            onPress={() => console.log("item pressed")}
-          >
-            <CardBody className="overflow-visible p-0">
-              <Image
-                shadow="sm"
-                radius="lg"
-                width="100%"
-                alt={item.title}
-                className="w-full object-cover h-[140px]"
-                src={item.img}
-              />
-            </CardBody>
-            <CardFooter className="text-lg justify-between">
-              <div className="flex flex-col w-full">
-                <div className="flex flex-row justify-between">
-                  <b className="text-yellow-400">{item.title}</b>
-                  <p className="text-default-500">{item.no_of_qs}</p>
+    <>
+
+      <section>
+        <div className="w-full px-4 sm:px-8 md:px-16 py-2 sm:py-4 md:py-8 bg-[#2D3250]">
+          <div className="grid gap-6 sm:gap-8 md:gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {list.map((item, index) => (
+              <Card
+                className="bg-[#1e243a] hover:bg-[#12172a] shadow-2xl"
+                radius="sm"
+                shadow="lg"
+                key={index}
+                isPressable
+                onPress={() => console.log("item pressed")}
+              >
+                <CardBody className="overflow-visible p-0">
+                  <Image
+                    shadow="sm"
+                    radius="lg"
+                    width="100%"
+                    alt={item.title}
+                    className="w-full object-cover h-[140px]"
+                    src={item.img}
+                  />
+                </CardBody>
+                <CardFooter className="text-lg justify-between">
+                  <div className="flex flex-col w-full">
+                    <div className="flex flex-row justify-between">
+                      <b className="text-yellow-400">{item.title}</b>
+                      {/* <p className="text-default-500">{item.no_of_qs}</p> */}
+                      <div class="relative flex items-center justify-center">
+                        <div class="absolute h-6 w-6 bg-primary-500 rounded-full"></div>
+                        <p class="text-default-200 text-[12px] z-10">{item.no_of_qs}</p>
+                      </div>
+
+
+                    </div>
+                    <div className="flex flex-row justify-between">
+                      <span className="text-[12px] text-slate-400">Total Score</span>
+                      <span className="text-[12px] text-slate-400">{item.no_of_qs}</span>
+                    </div>
+                  </div>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full grid grid-cols-2 px-4 sm:px-8 md:px-16 py-4 sm:py-8 md:py-12 bg-[#2D3250]">
+        {/* First Column */}
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3 bg-yellow-400">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://img.icons8.com/ios/50/clipboard.png"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">Notice</p>
+              <p className="text-small text-default-500">122</p>
+            </div>
+          </CardHeader>
+          <CardBody className="bg-[#1e243a]">
+            <p className="text-white">Welcome to the CTF Challenge! Get ready for an exciting journey into the world of cybersecurity. Sharpen your skills, think outside the box, and may the flags be ever in your favor!</p>
+          </CardBody>
+        </Card>
+
+        {/* Second Column - Duplicate of the First Column */}
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3 bg-primary-300">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://img.icons8.com/ios/50/conference-call--v1.png"
+              width={40}
+            />
+            <div className="flex">
+              <p className="text-md">Team</p>
+            </div>
+          </CardHeader>
+
+          <CardBody className="bg-[#1e243a]">
+            <Table
+              removeWrapper
+              aria-label="Example table with client side pagination"
+              bottomContent={
+                <div className="flex w-full justify-center">
+                  <Pagination
+                    isCompact
+                    showControls
+                    showShadow
+                    color="primary"
+                    page={page}
+                    total={pages}
+                    onChange={(page) => setPage(page)}
+                  />
                 </div>
-                <div className="flex flex-row justify-between">
-                  <span className="text-[12px] text-slate-400">Total Score</span>
-                  <span className="text-[12px] text-slate-400">{item.no_of_qs}</span>
-                </div>
-              </div>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    </div>
+              }
+              classNames={{
+        
+                wrapper: "min-h-[222px]",
+                table: "text-white",
+              }}
+            >
+              <TableHeader>
+                <TableColumn key="ctf_team_name">NAME</TableColumn>
+                <TableColumn key="score">SCORE</TableColumn>
+                <TableColumn key="finished_position">FINISHED</TableColumn>
+              </TableHeader>
+              <TableBody items={items}>
+                {(item) => (
+                  <TableRow key={item.ctf_team_name}>
+                    {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </CardBody>
+        </Card>
+      </section>
+
+    </>
+
   );
 }
-// <div>
-//   <section class="text-gray-600 body-font">
-//     <div class="container px-5 py-24 mx-auto">
-//       <div class="flex flex-wrap -m-4">
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-//             <p class="mt-1">$16.00</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-//             <p class="mt-1">$21.15</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-//             <p class="mt-1">$12.00</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-//             <p class="mt-1">$18.40</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-//             <p class="mt-1">$16.00</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-//             <p class="mt-1">$21.15</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-//             <p class="mt-1">$12.00</p>
-//           </div>
-//         </div>
-//         <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//           <a class="block relative h-48 rounded overflow-hidden">
-//             <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268" />
-//           </a>
-//           <div class="mt-4">
-//             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-//             <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-//             <p class="mt-1">$18.40</p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   </section>
-// </div>
-
-// <div class="bg-[#2D3250] text-white">
-//   <div class="p-4 mt-14">
-//     <div class="flex items-center justify-left h-20">
-//       <p class="text-3xl text-white">
-//         Your Stats
-//       </p>
-//     </div>
-//     <div class="flex items-center justify-left h-20">
-//       <p class="text-3xl text-white">
-//         Your Past Competitions
-//       </p>
-//     </div>
-//     <EventCard title={"Digital Cyber Pakistan"} organizer={"Ignite Inc"} date={"12-Dec-2023"} position={62} description={"Pakistans National Level Cyber Hackathon"} />
-//     <EventCard title={"Air University Hackathon"} organizer={"Air Uni"} date={"1-Dec-2023"} position={80} description={"Organzied by Air University Islamabad"} />
-
-//   </div>
-// </div>
-//   );
-// }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Image } from "@nextui-org/react";
 
 // import SidebarItem from "./sidebarItem";
 // import MultiSideBarItem from "./multiSideBarItem";
@@ -7,7 +8,7 @@ export default function Sidebar() {
     return (
         <aside
             id="logo-sidebar"
-            class="w-full h-screen pt-4 transition-transform -translate-x-full sm:translate-x-0 dark:bg-[#1B2137]"
+            class="w-full h-full pt-4 transition-transform -translate-x-full sm:translate-x-0 dark:bg-[#1B2137]"
             aria-label="Sidebar"
         >
             <div class="px-3 pb-4 overflow-y-auto bg-[#1B2137]">
@@ -17,14 +18,27 @@ export default function Sidebar() {
 
                     <li className="flex items-center px-2 py-4 transition duration-75 rounded-lg group hover:bg-yellow-400 dark:text-white dark:hover:bg-yellow-400">
                         <Link href="#" class="flex items-center w-full text-gray-900 transition duration-75 rounded-lg group">
-                            <img width="40" height="40" className="pr-[8px]" src="https://img.icons8.com/pastel-glyph/64/000000/box--v3.png" alt="box--v3" />
+                            <Image
+                                alt="nextui logo"
+                                height={40}
+                                radius="sm"
+                                src="https://img.icons8.com/arcade/64/flag.png"
+                                width={40}
+                                className="pr-[8px]"
+                            />
                             <p className="text-[12px] text-white hover:text-black hover:font-semibold">Digital Cyber Pakistan<br /><span className="text-xs pt-[4px] text-gray-400 line-clamp-1 hover:text-gray-500">This is platform where you solve ctf's</span></p>
                         </Link>
                     </li>,
                     <li className="flex items-center px-2 py-4 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-yellow-400">
                         <Link href="#" class="flex items-center w-full text-gray-900 ">
-                            <img width="40" height="40" className="pr-[8px]" src="https://img.icons8.com/pastel-glyph/64/000000/box--v3.png" alt="box--v3" />
-                            <p className="text-[12px] text-white hover:text-black hover:font-semibold">Digital Cyber Pakistan<br /><span className="text-xs pt-[4px] text-gray-400 line-clamp-1  hover:text-gray-500">This is platform where you solve ctf's</span></p>
+                            <Image
+                                alt="nextui logo"
+                                height={40}
+                                radius="sm"
+                                src="https://img.icons8.com/arcade/64/flag.png"
+                                width={40}
+                                className="pr-[8px]"
+                            />                                <p className="text-[12px] text-white hover:text-black hover:font-semibold">Digital Cyber Pakistan<br /><span className="text-xs pt-[4px] text-gray-400 line-clamp-1  hover:text-gray-500">This is platform where you solve ctf's</span></p>
                         </Link>
                     </li>,
 
