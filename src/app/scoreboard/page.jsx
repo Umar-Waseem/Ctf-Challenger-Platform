2c41d3f95c887c06272b68aedabe1d59435a4a36
Umar-Waseem <umar.waseem@gmail.com>
2023-12-04 20:41:29 +0500

    save user to db on signup

--- src/app/scoreboard/page.jsx
+++ src/app/scoreboard/page.jsx
@@ -1,31 +1,58 @@
+"use client"
+
 import React from 'react';
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
 function Scoreboard() {
-  const data = [
-    { name: 'John', score: 100 },
-    { name: 'Jane', score: 200 },
-    { name: 'Mike', score: 150 },
-    { name: 'Mike', score: 150 },
-    { name: 'Mike', score: 150 },
-    { name: 'Mike', score: 150 },
-    { name: 'Mike', score: 150 },
-  ];
+  const [data, setData] = React.useState([]);
+
+  const supabase = createClientComponentClient();
+
+  React.useEffect(() => {
+    const fetchScoreboard = async () => {
+      try {
+        const { data, error } = await supabase.from("users").select("*").order("total_score", { ascending: false })
+        console.log("Get Scoreboard: (error):\n", error);
+        console.log("Get Scoreboard: (data):\n", data);
+
+        if (data) {
+          setData(
+            data.map((item) => ({
+              id: item.id,
+              name: item.user_name,
+              score: item.total_score,
+            }))
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchScoreboard();
+
+    return () => {
+      console.log("Component unmounted");
+    };
+  }, []);
+
 
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold mb-4 text-yellow-400 py-10">Scoreboard</h1>
-        <div className="relative overflow-x-auto w-full">
+        <div className="relative overflow-x-auto w-3/4 mx-auto">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
               <tr>
-                <th scope="col" className="px-6 py-3 border-b border-gray-200">
+                <th scope="col" className="px-4 py-2 border-b border-gray-200">
                   Position
                 </th>
-                <th scope="col" className="px-6 py-3 border-b border-gray-200">
+                <th scope="col" className="px-4 py-2 border-b border-gray-200">
                   Name
                 </th>
-                <th scope="col" className="px-6 py-3 border-b border-gray-200">
+                <th scope="col" className="px-4 py-2 border-b border-gray-200">
                   Score
                 </th>
               </tr>
@@ -40,11 +67,11 @@ function Scoreboard() {
                       : `bg-white dark:bg-gray-800`
                   }
                 >
-                  <td className="px-6 py-4 border-b border-gray-200 font-medium text-gray-900 whitespace-nowrap dark:text-white">{index + 1}</td>
-                  <td className="px-6 py-4 border-b border-gray-200 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  <td className="px-4 py-4 border-b border-gray-200 font-medium text-gray-900 whitespace-nowrap dark:text-white">{index + 1}</td>
+                  <td className="px-4 py-4 border-b border-gray-200 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {item.name}
                   </td>
-                  <td className="px-6 py-4 border-b border-gray-200 font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.score}</td>
+                  <td className="px-4 py-4 border-b border-gray-200 font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.score}</td>
                 </tr>
               ))}
             </tbody>
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,22 +1,16 @@
 "use client"
 
-import React from 'react'
-
+import React from 'react';
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 function Users() {
-
     const supabase = createClientComponentClient();
-
     const [users, setUsers] = React.useState([]);
 
     React.useEffect(() => {
-        console.log("DB Request");
-
-        const fetchUsers = async (category) => {
+        const fetchUsers = async () => {
             try {
                 const { data, error } = await supabase.from("users").select("*")
-                // .eq("category", category);
                 console.log("Get Users: (error):\n", error);
                 console.log("Get Users: (data):\n", data);
 
@@ -38,10 +32,6 @@ function Users() {
         fetchUsers();
 
         return () => {
-            // Cleanup function
-            // Perform any necessary cleanup here
-            // For example, cancel any pending requests or subscriptions
-            // This function will be called when the component unmounts
             console.log("Component unmounted");
         };
     }, []);
@@ -50,7 +40,7 @@ function Users() {
         <>
             <div className="flex flex-col items-center justify-center h-screen">
                 <h1 className="text-4xl font-bold mb-4 text-yellow-400 py-10">Users</h1>
-                <div className="relative overflow-x-auto w-full">
+                <div className="relative overflow-x-auto w-4/5 mx-auto">
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
                             <tr>
@@ -93,7 +83,7 @@ function Users() {
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Users
+export default Users;
